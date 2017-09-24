d8c0222c730208ec750347ef428d7ac14675b78d
dmytro kozhukhar <dmytro.kozhukhar@outlook.com>
2017-09-24 22:15:38 +0300

    has enemy

--- Square Balls/main.cpp
+++ Square Balls/main.cpp
@@ -14,14 +14,14 @@ float tileSize = 50.f;
 
 std::string TileMap[10] = {
 	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBb",
-	"BBBB                    BBBB                                           B    B                                       B",
-	"B                                                                      B    B                                       B",
-	"B                   SSS                                 B              BBBBBB                  B                    B",
-	"BB                           BBBB                      BBB                                                         BB",
-	"B           BB               BBBBBBBBBB                  BBBBBBBBB                              B                   B",
-	"B                                 BBB                   B   B  B                                  B                 B",
-	"B                             BBBBB  BBB                B    B       B                            B    B            B",
-	"B                           BB BBBB    BBBB            B      B         B                           B    B          B",
+	"B                       BBBB                                           BBBBBB                                       B",
+	"B                                                                      BBBBBB                         SSS           B",
+	"BBBB                SSS                               BBB              BBBBBB                  BB                   B",
+	"B                            BBBBBBBBB     SSS        BBBB                                     BBBB                 B",
+	"B           BB               BBBBBBBBBB               BBBBBBBBBBBB                             BBBBBB               B",
+	"B        BBBBBBB             BBBBBBBBBB               BBBBBBBBBBBB                             BBBBBBBB        SSSSSB",
+	"B        BBBBBBB             BBBBBBBBBBB         SSSSSSSSSSBBBBBBBBBBB       SSS               BBBBBBBBBB      SSSSSB",
+	"B        BBBBBBB            BBBBBBBBBBBBBBB      SSSSSSSSSSBBBBBBBBBBBBBB                      BBBBBBBBBBB     SSSSSB",
 	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
 }
 ;
@@ -36,28 +36,16 @@ class Entity {
 public:
 	sf::Vector2f vel;
 	sf::RectangleShape shape;
+	bool onGround;
 
 
 	virtual void update() = 0;
 	void draw(sf::RenderWindow & window) {
 		window.draw(shape);
 	};
-};
 
-class Player: public Entity
-{
-public:
-	
-	bool onGround;
-
-	Player(sf::Vector2f pos = {0.f, 0.f}) {
-		shape.setPosition(pos);
-		shape.setSize({ tileSize*1.95f, tileSize*1.95f });
-		shape.setFillColor(sf::Color::Green);
-	}
-
-
-	void Collision(int dir) {
+	int Collision(int dir) {
+		int res = 0;
 		for (int i = shape.getPosition().y / tileSize; i < (shape.getPosition().y + shape.getSize().y) / tileSize; ++i)
 			for (int j = shape.getPosition().x / tileSize; j < (shape.getPosition().x + shape.getSize().x) / tileSize; ++j)
 			{
@@ -66,38 +54,107 @@ public:
 					if (TileMap[i][j] == 'B') {
 						if (vel.x > 0) {
 							shape.setPosition(j * tileSize - shape.getSize().x, shape.getPosition().y);
-							vel.x = 0;
+							vel.x = 0.f;
+							res += 1;
 						}
 						if (vel.x < 0) {
 							shape.setPosition(j * tileSize + tileSize, shape.getPosition().y);
-							vel.x = 0;
+							vel.x = 0.f;
+							res += 2;
 						}
 
 					}
-				} else if (dir == 1) {
-						if (TileMap[i][j] == 'B') {
-							if (vel.y > 0) {
-								shape.setPosition(shape.getPosition().x, i * tileSize - shape.getSize().y);
-								vel.y = 0;
-								onGround = true;
-
-							}
-							if (vel.y < 0) {
-								shape.setPosition(shape.getPosition().x, i * tileSize + tileSize);
-								vel.y = 0;
-							}
-
+				}
+				else if (dir == 1) {
+					if (TileMap[i][j] == 'B') {
+						if (vel.y > 0) {
+							shape.setPosition(shape.getPosition().x, i * tileSize - shape.getSize().y);
+							vel.y = 0;
+							onGround = true;
+							res += 4;
 						}
+						if (vel.y < 0) {
+							shape.setPosition(shape.getPosition().x, i * tileSize + tileSize);
+							vel.y = 0;
+							res += 8;
+						}
+
+					}
 
 				}
 
 
 				if (TileMap[i][j] == 'S') {
 					TileMap[i][j] = ' ';
+					res += 16;
 				}
 
 			}
+		return res;
 	};
+};
+
+
+class Enemy : public Entity
+{
+public:
+
+	bool alive;
+
+	Enemy(sf::Vector2f pos = { 0.f, 0.f }) {
+		shape.setPosition(pos);
+		shape.setSize({ tileSize*0.95f, tileSize*0.95f });
+		shape.setFillColor(sf::Color::Blue);
+		alive = true;
+		vel.x = 1.f;
+	}
+
+
+	void update() {
+
+
+		shape.move(vel.x, 0);
+
+		int isHit = Collision(0);
+
+		//if (onGround)
+		if ((isHit & 1) == 1)
+			vel.x = -1.f;
+		if ((isHit & 2) == 2)
+			vel.x = 1.f;
+		//else
+		//vel.x *= rub * 1.5f;
+
+
+		if (!onGround && shape.getPosition().y >= ground) {
+			shape.move(0, ground - shape.getPosition().y);
+			onGround = true;
+			vel.y = 0;
+		}
+		else {
+			onGround = false;
+			vel.y += gt;
+			shape.move(0, vel.y);
+		}
+
+		Collision(1);
+
+	};
+
+
+};
+
+class Player: public Entity
+{
+public:
+	
+	
+
+	Player(sf::Vector2f pos = {0.f, 0.f}) {
+		shape.setPosition(pos);
+		shape.setSize({ tileSize*1.95f, tileSize*1.95f });
+		shape.setFillColor(sf::Color::Green);
+	}
 
 
 	void update() {
@@ -126,7 +183,7 @@ public:
 
 		Collision(1);
 
-	}
+	};
 
 };
 
@@ -145,7 +202,7 @@ int main() {
 	window.setView(view);
 
 
-
+	Enemy goodboy({ 800,300 });	
 
 	Player hero({ 250,250 });
 
@@ -177,7 +234,12 @@ int main() {
 			hero.vel += (down);
 		}
 
+		goodboy.update();
 		hero.update();
+
+		if (hero.shape.getGlobalBounds().intersects(goodboy.shape.getGlobalBounds())) {
+			hero.shape.setFillColor(goodboy.shape.getFillColor());
+		}
 
 		view.setCenter(hero.shape.getPosition());
 		window.setView(view);
@@ -205,7 +267,7 @@ int main() {
 			}
 		}
 
-
+		goodboy.draw(window);
 		hero.draw(window);
 
 		window.display();
